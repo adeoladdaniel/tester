6dd4f76bf8601124283bacd2dd1d4080e6bb4678
adeola daniel <adeoladaniel@adeolas-MBP.lan>
2021-10-18 13:28:45 +0100

    testelola

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function calculate() {
       const rate = data.conversion_rates[currency_two];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+      amountEl_two.value = (amountEl_one.value * 50 * rate).toFixed(2);
     });
 }
 
